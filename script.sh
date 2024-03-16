12ba9a39180c00bae7af443e7faf48f0a8539cc4
ant <ant@ant.ant>
2024-03-16 19:52:56 +0100

    fixes

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install git python screen
-cd .
+sudo apt-get install git python3 screen
+cd ~
 mkdir codeproj
 mkdir git-stuff
 touch info.txt
